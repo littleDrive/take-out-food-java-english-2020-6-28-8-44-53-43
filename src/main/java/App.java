64e7345b52d3daca07fdf4699ce8a657c10d9f82
littleDrive <2682155146@qq.com>
2020-06-29 03:31:53 +0800

    second commit

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,4 @@
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 
 /*
@@ -75,7 +74,7 @@ public class App {
         }
         result += "-----------------------------------\n";
 
-        if (false) {
+        if (flag) {
 
 
                     result += "Total：" + total1 + " yuan\n" +
